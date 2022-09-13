b9f55c259c3316d596fa09585415257a7d1a3882
Luis <luis.sulecio@gmail.com>
2022-09-13 03:07:36 -0600

    Arreglos en el monitor
    Monitor esta recibiendo mensajes desde el DUT pero no funciona correctamente
    Faltan agregar fifos simuladas
    Falta agregar test que pruebe todos los drivers
    Tambien se podria agregar test que mande a la misma direccion u otros que sean faciles de hacer rapido

--- Monitor.sv
+++ Monitor.sv
@@ -1,26 +1,27 @@
 class monitor #(parameter pckg_size, num_msg, drvrs, bits);
 
 
-    trans_bus #(.pckg_size(pckg_size), .drvrs(drvrs)) msg_2_Monitor;
+  trans_bus #(.pckg_size(pckg_size), .drvrs(drvrs)) msg_2_Monitor[drvrs-1:0];
     virtual bus_if #(.bits(bits), .drvrs(drvrs), .pckg_size(pckg_size)) bus_interface;
     
   
   task run();
     $display("Monitor correctamente inicializado");
 
-    for ( int i=0; i < drvrs; i++)begin
-      
+    for ( int j=0; j < drvrs; j++)begin
+      	automatic int i=j;
         fork
+          msg_2_Monitor[i]=new;
             forever begin//
-              @(posedge bus_interface.clk)
-              if (bus_interface.push[0][i])begin
+              @(posedge bus_interface.push[0][i])
                 $display("push en la salida: ", bus_interface.push[0][i]);
                 //falta la parte donde el dato entra a la fifo simulada y tambien sale de esta
-                msg_2_Monitor.message<=bus_interface.D_push[0][i]; 
-                $display("Agente"); 
-                $display("Se obtuvo el mensaje: %b",msg_2_Monitor.message);
+                msg_2_Monitor[i].message<=bus_interface.D_push[0][i]; 
+              	$display(""); 
+              $display("------Monitor-----"); 
+                $display("Se obtuvo el mensaje: %b",msg_2_Monitor[i].message);
                 $display("en el dispositivo", i);
-              end
+     
 
             end
         join_none
