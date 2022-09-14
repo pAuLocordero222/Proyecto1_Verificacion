d40ce48258f156c5e0257f228d573ec0194a0f16
Luis <luis.sulecio@gmail.com>
2022-09-14 03:51:05 -0600

    Update Driver.sv

--- Driver.sv
+++ Driver.sv
@@ -12,15 +12,14 @@ class driver #(parameter pckg_size, num_msg, drvrs, bits);
 
 
   task run();
-
+    fork
       for ( int i=0; i < drvrs; i++)begin
           fifo[i]=new();
           #1
-          fork
-            $display("olaaaa");
-            fifo[i].run();
-          join_none
+          $display("olaaaa");
+          fifo[i].run();
       end
+    join_none
 
     $display("Esto esta pasando");
     //$display("Mensaje en driver:", msg_2_DUT.payload);//se obtiene el mensaje que se envio desde el agente
