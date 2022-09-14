6fd674433ddc8d54f41443ced191e25713f7ed63
Luis <luis.sulecio@gmail.com>
2022-09-14 04:01:49 -0600

    Update Driver.sv

--- Driver.sv
+++ Driver.sv
@@ -21,7 +21,7 @@ class driver #(parameter pckg_size, num_msg, drvrs, bits);
 
     for ( int p=0; p < drvrs; p++)
       begin//se recorren con el numero de dispositivos
-        fifo[j]=new();
+        fifo[i]=new();
         fork 
           automatic int j=p;
           
