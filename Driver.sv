c654ad76c27994d019646749acdc40e9821bbae1
Luis <luis.sulecio@gmail.com>
2022-09-14 03:12:59 -0600

    Update Driver.sv

--- Driver.sv
+++ Driver.sv
@@ -12,7 +12,7 @@ class driver #(parameter pckg_size, num_msg, drvrs, bits);
 
     initial begin
       for ( int i=0; p < drvrs; i++)begin
-          ifo[i]=new();
+          fifo[i]=new();
           fifo[i].run();  
       end
     end
