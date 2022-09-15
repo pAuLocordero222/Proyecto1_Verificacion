d2c0c7125491dd27e8cd816fc359aeb2d0f93232
Luis <luis.sulecio@gmail.com>
2022-09-15 03:57:10 -0600

    Update Monitor.sv

--- Monitor.sv
+++ Monitor.sv
@@ -14,7 +14,7 @@ class monitor #(parameter pckg_size, num_msg, drvrs, bits);
           automatic int i=j;
           msg_2_Monitor[i]=new;
             forever begin//
-              @(posedge vif.clk)
+              @(negedge vif.clk)
               if (vif.push[0][i]==1)begin
                 $display("push en la salida: ", vif.push[0][i]);
                 //falta la parte donde el dato entra a la fifo simulada y tambien sale de esta
