ab3b7b7ba53881e11ee6d659fe31f5340ea9fd8a
Luis <luis.sulecio@gmail.com>
2022-09-15 03:50:51 -0600

    Update Testbench.sv

--- Testbench.sv
+++ Testbench.sv
@@ -16,9 +16,9 @@ module tb;
 
 
   parameter pckg_size = 16;
-  parameter drvrs = 4;
+  parameter drvrs = 1;
   parameter bits = 1;
-  parameter num_msg = 1;
+  parameter num_msg = 4;
 
   tipo_test test;
   reg clk;
