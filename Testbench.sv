755c6f0b7974e37cd32c0bc5cb964463f39196fb
Luis <luis.sulecio@gmail.com>
2022-09-15 03:24:12 -0600

    a

--- Testbench.sv
+++ Testbench.sv
@@ -18,7 +18,7 @@ module tb;
   parameter pckg_size = 16;
   parameter drvrs = 4;
   parameter bits = 1;
-  parameter num_msg = 10;
+  parameter num_msg = 4;
 
   tipo_test test;
   reg clk;
--- Trans.sv
+++ Trans.sv
@@ -28,12 +28,14 @@ class Fifo #(parameter pckg_size, drvrs, bits);
             vif.pndng[0][k] <= 1'b0;
             forever begin
                 
+                
                 @(posedge vif.clk)
                 vif.D_pop[0][k]= q[0];
-                               
+                    $display("D_pop en %0d es:%0b",k, vif.D_pop[0][k]);
+                    $display("pndng %0d esta en %0d",k, vif.pndng[0][k               
                    if(q.size()>0) begin
     
-                        #5 vif.pndng[0][k]= 1'b1;
+                        vif.pndng[0][k]= 1'b1;
                         
                        /* $display("contenido en fifo %0d es de %0d",k, q.size());
                         $display("contenido en la primera posicion de la fifo %0d es %0b",k, q[0]);
