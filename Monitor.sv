f997e9497339f81602652c9ab6972006bb0d2b30
pAuLocordero222 <88518574+pAuLocordero222@users.noreply.github.com>
2022-09-15 01:52:14 -0600

    Update Monitor.sv

--- Monitor.sv
+++ Monitor.sv
@@ -1,41 +1,35 @@
 class monitor #(parameter pckg_size, num_msg, drvrs, bits);
 
-
   mailbox mntr_2_chckr_mbx;
 
   trans_bus #(.pckg_size(pckg_size), .drvrs(drvrs)) msg_2_Monitor[drvrs-1:0];
-    virtual bus_if #(.bits(bits), .drvrs(drvrs), .pckg_size(pckg_size)) bus_interface;
+    virtual bus_if #(.bits(bits), .drvrs(drvrs), .pckg_size(pckg_size)) vif;
     
   
   task run();
     $display("Monitor correctamente inicializado");
 
     for ( int j=0; j < drvrs; j++)begin
-      	automatic int i=j;
+      	
         fork
+          automatic int i=j;
           msg_2_Monitor[i]=new;
             forever begin//
-              @(posedge bus_interface.push[0][i])
-                $display("push en la salida: ", bus_interface.push[0][i]);
+              @(posedge vif.clk)
+              if (vif.push[0][i]!=0)begin
+                $display("push en la salida: ", vif.push[0][i]);
                 //falta la parte donde el dato entra a la fifo simulada y tambien sale de esta
-                msg_2_Monitor[i].message<=bus_interface.D_push[0][i]; 
                 msg_2_Monitor[i].message<=vif.D_push[0][i]; 
                 mntr_2_chckr_mbx.put(msg_2_Monitor[i]);
               	$display(""); 
-              $display("------Monitor-----"); 
+                $display("------Monitor-----"); 
                 $display("Se obtuvo el mensaje: %b",msg_2_Monitor[i].message);
                 $display("en el dispositivo", i);
+              end
      
 
             end
         join_none
-
-    end
-
-
-
-
-    
-  endtask
-  
+    end   
+  endtask  
 endclass
