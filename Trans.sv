a1528017ddd6e3ebddac6e9c0e9559df814ac094
Luis <luis.sulecio@gmail.com>
2022-09-15 04:54:57 -0600

    Update Trans.sv

--- Trans.sv
+++ Trans.sv
@@ -35,17 +35,15 @@ class Fifo #(parameter pckg_size, drvrs, bits);
                     $display("pndng %0d esta en %0d",k, vif.pndng[0][k]);
                     $display("push %0d esta en %0d",k, vif.push[0][k]);
                     $display("");*/              
-                   if(q.size()>0) begin
-                    if (cont!=0)begin
+                   if(q.size()!=0) begin
                         vif.D_pop[0][k]= q[0];
                         vif.pndng[0][k]= 1'b1;
                         $display(cont);
-                        $display("contenido en fifo %0d es de %0d",k, q.size());
+                        /*$display("contenido en fifo %0d es de %0d",k, q.size());
                         $display("contenido en la primera posicion de la fifo %0d es %0b",k, q[0]);
                         $display("D_pop en %0d es:%0b",k, vif.D_pop[0][k]);
                         $display("pndng %0d esta en %0d",k, vif.pndng[0][k]);
-                        end 
-                    end
+                        end*/
                     //Fifo vacia
                     else begin
                         vif.pndng[0][k] <= 1'b0;
