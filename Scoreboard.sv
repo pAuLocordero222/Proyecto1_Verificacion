ea7b8986f9dc8b36dda60d039ffaae6a2aa6da54
pAuLocordero222 <88518574+pAuLocordero222@users.noreply.github.com>
2022-09-21 02:33:37 -0600

    Update Scoreboard.sv

--- Scoreboard.sv
+++ Scoreboard.sv
@@ -30,7 +30,7 @@ class scoreborad #(parameter pckg_size, num_msg, drvrs, bits);
                     msg_chckr_scrbrd[j] = new();
 
                     chckr_2_scrbrd_mbx.get(msg_chckr_scrbrd[j]);
-                    if (msg_drvr_scrbrd[k]==msg_chckr_scrbrd[j]) begin
+                    if (msg_drvr_scrbrd[k].message==msg_chckr_scrbrd[j].message) begin
                         t_total = t_total + msg_chckr_scrbrd[j].tiempo_recibido - msg_drvr_scrbrd[k].tiempo_envio;
                     end
                 end
