13d9f192a2f7498915ce1bd12e6f6d12c759f3c1
pAuLocordero222 <88518574+pAuLocordero222@users.noreply.github.com>
2022-09-21 19:30:24 -0600

    Update Checker.sv

--- Checker.sv
+++ Checker.sv
@@ -1,60 +1,48 @@
 class checker #(parameter pckg_size, num_msg, drvrs, bits);
 
-    bit [pckg_size-1:0]q_chkr[$]={};
+    bit [pckg_size-1:0]q_chkr[$]={}; //Queue para guardar los mensajes del monitor
 
+    //Definicion de mailboxes
     mailbox mntr_2_chckr_mbx;
     mailbox agnt_2_chckr_mbx;
     mailbox chckr_2_scrbrd_mbx;
 
 
-    //event AgenteListo;  
+    //Objetos usados para el checker  
     trans_bus #(.pckg_size(pckg_size), .drvrs(drvrs)) msg_agnt_chckr;
     trans_bus #(.pckg_size(pckg_size), .drvrs(drvrs)) msg_mntr_chckr;
     
     task run();
-        #100;
         $display("[%g] El checker fue inicializado.", $time);
-        msg_agnt_chckr = new;
-        msg_mntr_chckr = new;
+        msg_agnt_chckr = new;//Inicializacion del objeto
+        msg_mntr_chckr = new;//Inicializacion del objeto
+
+        //Ciclo for para guardar los mensajes del monitor en el queue
         for (int i = 0; i < num_msg; i++) begin
-            mntr_2_chckr_mbx.get(msg_mntr_chckr);
-            chckr_2_scrbrd_mbx.put(msg_mntr_chckr);
-            q_chkr.push_back(msg_mntr_chckr.message);
+            mntr_2_chckr_mbx.get(msg_mntr_chckr);//Toma los mensajes proveinentes del monitor
+            chckr_2_scrbrd_mbx.put(msg_mntr_chckr);//Envia los mensajes recibidos en el monitor al scoreboard
+            q_chkr.push_back(msg_mntr_chckr.message);//Guarda los mensajes en un queue
         end
 
 
-        for (int i = 0; i < num_msg; i++) begin
-            //mntr_2_chckr_mbx.get(msg_mntr_chckr); //Se obtiene el dato desde el monitor
+        for (int i = 0; i < num_msg; i++) begin//for para tomar todos los mensajes generados en el agente
             agnt_2_chckr_mbx.get(msg_agnt_chckr); //Se obtiene el dato desde el agente
             $display("---------------------------");
             $display("Checker");
             $display("Se recibieron los mensajes desde el agente y desde el monitor");
-            //$display("Mensaje del agente: %b", msg_agnt_chckr.message);
-            //$display("Mensaje del monitor: %b", msg_mntr_chckr.message);
             
 
-            for (int j = 0; j < $size(q_chkr); j++) begin
-                if (q_chkr[j] == msg_agnt_chckr.message) begin
-                    $display("La transaccion %0d fue realizada correctamente", i);
+            for (int j = 0; j < $size(q_chkr); j++) begin//Se recorre el queue con los mensajes del monitor
+                if (q_chkr[j] == msg_agnt_chckr.message) begin//Se compara el mensaje del queue con el tomado del agente
+                    $display("La transaccion %0d fue realizada correctamente", i); //Se imprime esto si los mensajes coinciden
                     $display("---------------------------");
                 end
             end
 
-            /*
-            $display("La transaccion %0d fue realizada correctamente", i);
-            $display("---------------------------");*/
-
-/*
-            else begin
-                $display("ERROR: no se recibio el mensaje %d correctamente", i);
-                $display("---------------------------");
-            end*/
 
         end
 
-        
-
-
+    
     endtask
 
 
