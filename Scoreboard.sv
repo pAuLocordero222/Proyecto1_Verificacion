21fb465dcb37237dc642d22031bd25f7b4c877d2
pAuLocordero222 <88518574+pAuLocordero222@users.noreply.github.com>
2022-09-22 00:19:46 -0600

    Update Scoreboard.sv

--- Scoreboard.sv
+++ Scoreboard.sv
@@ -10,8 +10,8 @@ class scoreborad #(parameter pckg_size, num_msg, drvrs, bits);
     int t_promedio = 0;
     int fcsv;
 
-    trans_bus #(.pckg_size(pckg_size), .drvrs(drvrs)) msg_drvr_scrbrd[num_msg-1:0];
-    trans_bus #(.pckg_size(pckg_size), .drvrs(drvrs)) msg_chckr_scrbrd[num_msg-1:0];
+    trans_bus #(.pckg_size(pckg_size), .drvrs(drvrs)) msg_drvr_scrbrd;//[drvrs-1:0];
+    trans_bus #(.pckg_size(pckg_size), .drvrs(drvrs)) msg_chckr_scrbrd;//[drvrs-1:0];
 
     task run();
         
@@ -24,10 +24,8 @@ class scoreborad #(parameter pckg_size, num_msg, drvrs, bits);
 
         $display("Retardo promedio");
 
-        for (int i = 0; i < num_msg; i++) begin
-            msg_drvr_scrbrd[i] = new();
-            msg_chckr_scrbrd[i] = new();
-        end
+        msg_drvr_scrbrd = new;
+        msg_chckr_scrbrd = new;
 
 
 
@@ -36,7 +34,7 @@ class scoreborad #(parameter pckg_size, num_msg, drvrs, bits);
                 automatic int k = i;
                 //*msg_drvr_scrbrd[k] = new();
 
-                drvr_2_scrbrd_mbx.peek(msg_drvr_scrbrd[k]);
+                drvr_2_scrbrd_mbx.peek(msg_drvr_scrbrd);
 
                 
                 $fwrite(fcsv, "%d, %d, %d, %d, %d \n", 1,2,3,4,5);
@@ -46,8 +44,8 @@ class scoreborad #(parameter pckg_size, num_msg, drvrs, bits);
                     //$display("%0h", msg_drvr_scrbrd[k].message);
                     //msg_chckr_scrbrd[j] = new();
 
-                    chckr_2_scrbrd_mbx.peek(msg_chckr_scrbrd[j]);
-                    if (msg_drvr_scrbrd[k].message==msg_chckr_scrbrd[j].message) begin
+                    chckr_2_scrbrd_mbx[j].peek(msg_chckr_scrbrd);
+                    if (msg_drvr_scrbrd.message==msg_chckr_scrbrd.message) begin
                         drvr_2_scrbrd_mbx.get(msg_drvr_scrbrd[k]);
                         chckr_2_scrbrd_mbx.get(msg_chckr_scrbrd[j]);
                         $display("%0d", msg_drvr_scrbrd[k].tiempo_envio);
